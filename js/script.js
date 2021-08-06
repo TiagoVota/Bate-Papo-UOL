6009522c56641b50a5d4cb768418f50f8808dbee
Tiagovota <tiagovotacucco@gmail.com>
2021-08-06 01:33:40 -0300

    Adicionadas funções para carregar mensagens do servidor

--- js/script.js
+++ js/script.js
@@ -1,1 +1,64 @@
-// Arquivo de códigos Js
+const URL_MESSAGES = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages'
+
+
+function loadMessages() {
+    const mainElement = document.querySelector('main')
+    const oldMessageElement = mainElement.lastElementChild
+
+    axios.get(URL_MESSAGES)
+    .then((messages) => {
+        clearMain(mainElement)
+
+        for (const message of messages.data) {
+            
+            const messageElement = makeMessageElement(message)
+            addMessage(messageElement, mainElement)
+        }
+
+        const newMessageElement = mainElement.lastElementChild
+        if (haveNewMessage(oldMessageElement, newMessageElement)) {
+            // ESSA FUNÇÃO TEM QUE VIR PRIMEIRO, ESTOU RENDERIZANDO O DOM INTEIRO A TOA
+            scrollToLastMessage(mainElement)
+        }
+
+    })
+}
+
+
+function makeMessageElement({ from, to, text, type, time }) {
+    const messageElement = `<div class="msg-${ type }">
+        <p>
+            <span>(${ time })</span> &ensp;<strong>${ from }</strong> para <strong>${ to }</strong>: ${ text }
+        </p>
+    </div>
+    `
+    return messageElement
+}
+
+
+function scrollToLastMessage(mainElement) {
+    mainElement.lastElementChild.scrollIntoView({ behavior: 'smooth' })
+}
+
+
+function addMessage(messageElement, mainElement) {
+    mainElement.innerHTML += messageElement
+}
+
+
+function clearMain(mainElement) {
+    mainElement.innerHTML = ''
+}
+
+
+function haveNewMessage(oldMessageElement, newMessageElement) {
+    if (oldMessageElement === null) return true  // Caso primeira vez carregado
+
+    const oldTime = oldMessageElement.querySelector('span').innerText
+    const newTime = newMessageElement.querySelector('span').innerText
+
+    if (oldTime !== newTime) return true
+    return false
+}
+
+setInterval(loadMessages, 3000)
